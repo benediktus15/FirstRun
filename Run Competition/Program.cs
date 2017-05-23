651fcc710abb9e3735a5f6627b427852463879e1
Benediktus Zebua <benediktus@magang.indovti.com>
2017-05-23 11:40:39 +0700

    Update-01

---
+++ Run Competition/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Run_Competition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            int total=0;
+
+            // Looping
+            while (true)
+            {
+                // Keluarkan pertanyaan
+                Console.WriteLine("Berapa Lama Berolahraga ?");
+
+                // Minta inout dari user 
+                input = Console.ReadLine();
+
+                // Menambahkan inputny
+                total = total + Int32.Parse(input);                                
+                                 
+                // Keluar bila user input "quit"
+                if (input == "quit")
+                {
+                    break;
+                }
+
+                // Keluar inputnya
+                Console.WriteLine("anda berolahraga sebanyak " + input + " menit. ");
+                
+            }
+
+
+        }
+
+       
+    }
+}
